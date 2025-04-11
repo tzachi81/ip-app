a4cbb802ea62dbe0106ec9930b3ee4970b70773a
tzachi81 <tzahih@gmail.com>
2025-04-11 17:07:28 +0300

    Fix origin key

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const devOrigins = ['http://localhost:5173', 'http://localhost:4173'];
-const allowedOrigins = (process.env.NODE_ENV === "production") ? [process.env.allowedOrigins] : devOrigins;
+const allowedOrigins = (process.env.NODE_ENV === "production") ? [process.env.ALLOWED_ORIGIN] : devOrigins;
 
 const corsOptions = {
     origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
